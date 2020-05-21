a4f93d20b3340f67e26a973d46125a27631138c4
Arbaletos <artemioarbaletos@gmail.com>
2020-05-21 06:46:54 +0300

    incapsulating demandanto in a class

--- demandanto.py
+++ demandanto.py
@@ -2,17 +2,32 @@ import sys
 
 from morph import inflect, multi_inflect
 
-def get_rel(kb, rel, arg1=None, arg2=None):
-    ret = []
-    for r, args in kb:
-        if r!=rel:
-            continue
-        if arg1 is not None and arg1 != args[0]:
-            continue
-        if len(args)>1 and arg2 is not None and arg2 != args[1]:
-            continue
-        ret.append((r, args))
-    return ret
+
+class Demandanto:
+  
+    def __init__(self, kb_path):
+        self.kb = []
+        with open(inputo, 'r', encoding='utf-8') as reader:
+            for line in reader:
+                line = line.strip()
+                if len(line)==0 or ':-' in line:
+                    continue
+                pred = line[:line.index('(')]
+                args = line[len(pred)+1:-2].replace("'", '').split(',')
+                self.kb.append((pred, args))
+
+
+    def get_rel(self, rel, arg1=None, arg2=None):
+        ret = []
+        for r, args in self.kb:
+            if r!=rel:
+                continue
+            if arg1 is not None and arg1 != args[0]:
+                continue
+            if len(args)>1 and arg2 is not None and arg2 != args[1]:
+                continue
+            ret.append((r, args))
+        return ret
 
 
 if __name__=='__main__':
@@ -22,13 +37,7 @@ if __name__=='__main__':
 
     inputo = sys.argv[1]
 
-    kb = []
-    with open(inputo, 'r', encoding='utf-8') as reader:
-        for line in reader:
-            line = line.strip()
-            if len(line)==0 or ':-' in line:
-                continue
-            pred = line[:line.index('(')]
-            args = line[len(pred)+1:-2].replace("'", '').split(',')
-            kb.append((pred, args))
-    print(kb)
+    demandanto = Demandanto(inputo)
+    print(demandanto.kb)
+
+    print(demandanto.get_rel('hyp', None, 'Формальный язык'))
